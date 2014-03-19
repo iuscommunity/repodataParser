d20014f9b2d311a93b8216eebdd44db872d7b4c7
cgtx <carl@carlgeorge.us>
2014-03-19 15:49:09 -0500

    pep8 whitespace fixes

--- repodataParser/RepoParser.py
+++ repodataParser/RepoParser.py
@@ -3,9 +3,10 @@ from StringIO import StringIO
 from gzip import GzipFile
 from xml.dom import minidom
 
+
 class Parser:
     'A class for reading a Yum repos primary.xml.gz and returning data'
-    
+
     def __init__(self, *args, **kwargs):
         'Our initializer for the class'
         if kwargs.has_key('url'):
@@ -17,36 +18,36 @@ class Parser:
             self.__todict()
         else:
             raise Exception('kwargs url missing')
-    
+
     def __open(self):
         'Open a Yum Repodata XML File'
         self.res = urlopen('%s' % self.url).read()
-        
+
     def __decompress(self):
         'Attempts to decompress a string as Gzip'
         buf = StringIO(self.res)
         f = GzipFile(fileobj=buf)
         try:
-             self.content = f.read()
+            self.content = f.read()
         except IOError:
             self.content = self.res
-    
+
     def __dom(self):
         'get the XML dom object'
         self.dom = minidom.parse(StringIO(self.content))
-        
+
     def __elements(self):
         'Get a Element by ID name'
         self.elements = self.dom.getElementsByTagName('package')
-            
+
     def __node_get_attributes(self, node):
         if node.attributes.items():
             return dict(node.attributes.items())
-    
+
     def __node_get_value(self, node):
         if node.firstChild:
             return node.firstChild.nodeValue
-        
+
     def __todict(self):
         'create a dict from the XML data'
         data = []
@@ -61,11 +62,11 @@ class Parser:
                         object[key] = (value, attr)
                 data.append(object)
         self.data = data
-        
+
     def getList(self):
         'returns a python list of dicts of the nodes in a XML files TagName'
         return self.data
-    
+
     def getPackage(self, package):
         'return a python list of dicts for a package name'
         mypackages = []
